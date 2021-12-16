25e494f0186bd3e2d6703705cd1beb6527cfb2b7
JakobParpart <92376506+JakobParpart@users.noreply.github.com>
2021-12-16 07:54:16 -0500

    Update Tutorial_5_Runway_LEDs.cpp

--- Tutorial_5_Runway_LEDs.cpp
+++ Tutorial_5_Runway_LEDs.cpp
@@ -38,7 +38,7 @@ with no power.                                                                  
 const int analogPin = A0;   // the pin that the potentiometer is attached to
 const int ledCount = 10;    // the number of LEDs in the bar graph
 
-int ledPins[] = {
+int ledPins[] = { //declares each pin to an LED
  2, 3, 4, 5, 6, 7, 8, 9, 10, 11 // an array of pin numbers to which LEDs are attached
 };   // end of LED pin declaration
 
